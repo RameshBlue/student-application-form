037bc4ca269c16401b054c2b48c10135d6c3a5b5
Ramesh Krishnan <Ramesh_Krishnan2@comcast.com>
2022-12-19 11:02:01 +0530

    photo upload changes

--- src/components/PageFour.jsx
+++ src/components/PageFour.jsx
@@ -22,10 +22,10 @@ const PageFour = ({ isComplete }) => {
 
     const ValidationSchema = Yup.object().shape({
         photo: Yup.mixed().required("Photo is required").test("fileSize", "File size is too large", function (value) {
-            return value != null && value.size <= 200000;
+            return value != null && value.size <= 1000000;
         }),
         signature: Yup.mixed().required("Photo is required").test("fileSize", "File size is too large", function (value) {
-            return value != null && value.size <= 200000;
+            return value != null && value.size <= 1000000;
         })
     })
 
@@ -48,11 +48,11 @@ const PageFour = ({ isComplete }) => {
                     <div className='flex flex-col text-center items-center rounded-xl shadow-[1px_3px_7px_0.05px_rgba(0,0,0,0.15)] border-[2px] border-dashed border-gray-400 p-12 cursor-pointer' onClick={() => { photoRef.current.click() }}>
                         <img className='w-8 h-8 mb-2' src={UploadIcon} alt="uploadicon" />
                         <p className='mb-1 mt-1 font-semibold'>Upload Image</p>
-                        <p className='text-gray-600'>Max Size: 2MB</p>
+                        <p className='text-gray-600'>Max Size: 1MB</p>
                     </div>
                     <input className='hidden' name="photo" type="file" accept='image/*' ref={photoRef} onChange={(e) => {
-                        setPhotoName(e.target.files[0].name);
                         setFieldValue("photo", e.target.files[0]);
+                        e.target.files[0].size <= 1000000 ? setPhotoName(e.target.files[0].name) : setPhotoName(undefined);
                     }} />
                     <p>{photoName}</p>
                     {errors.photo ? <p className='text-red-600'>{errors.photo}</p> : null}
@@ -62,11 +62,11 @@ const PageFour = ({ isComplete }) => {
                     <div className='flex flex-col text-center items-center rounded-xl shadow-[1px_3px_7px_0.05px_rgba(0,0,0,0.15)] border-[2px] border-dashed border-gray-400 p-12 cursor-pointer' onClick={() => { signatureRef.current.click() }}>
                         <img className='w-8 h-8 mb-2' src={UploadIcon} alt="uploadicon" />
                         <p className='mb-2 mt-1 font-semibold'>Upload Image</p>
-                        <p className='text-gray-600'>Max Size: 2MB</p>
+                        <p className='text-gray-600'>Max Size: 1MB</p>
                     </div>
                     <input className='hidden' name="signature" type="file" accept='image/*' ref={signatureRef} onChange={(e) => {
-                        setSigantureName(e.target.files[0].name);
                         setFieldValue("signature", e.target.files[0]);
+                        e.target.files[0].size <= 1000000 ? setSigantureName(e.target.files[0].name) : setSigantureName(undefined);
                     }} />
                     <p>{sigantureName}</p>
                     {errors.signature ? <p className='text-red-600'>{errors.signature}</p> : null}
