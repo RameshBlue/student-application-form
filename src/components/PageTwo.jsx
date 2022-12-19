3f9f99191268828fe1a8b1c1dd7ce4e3a5b1a8d6
Ramesh Krishnan <Ramesh_Krishnan2@comcast.com>
2022-12-19 10:48:02 +0530

    commit 19th dec

--- src/components/PageTwo.jsx
+++ src/components/PageTwo.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { pageContext } from '../context/PageContext'
+import moment from "moment";
 
 const Boards = ["Select", "State Board", "CBSE", "ICSE"]
 
@@ -17,17 +18,17 @@ const PageTwo = ({ isComplete }) => {
 
     const ValidationSchema = Yup.object().shape({
         schoolName: Yup.string().required("School name is required"),
-        yearGraduated: Yup.string().required("Graduated year is required"),
+        dateGraduated: Yup.date().required("Graduated date is required").max(new Date(Date.now()),"You are not graduated yet"),
         board: Yup.string().test("board", "Select a board", function (value) {
             return value != "" && value != "Select";
         }).required("Select a board"),
-        mark: Yup.number().typeError("Mark should be in numbers").min(0,"Mark should be greater than or equal to zero").max(100,"Mark should be less than or equal to 100").required("Total mark is required")
+        mark: Yup.number().typeError("Mark should be in numbers").min(0, "Mark should be greater than or equal to zero").max(100, "Mark should be less than or equal to 100").required("Total mark is required")
     })
 
     const { handleBlur, handleChange, handleSubmit, values, errors, touched } = useFormik({
         initialValues: {
             schoolName: "",
-            yearGraduated: "",
+            dateGraduated: "",
             board: "",
             mark: ""
         },
@@ -59,9 +60,9 @@ const PageTwo = ({ isComplete }) => {
                     {errors.board && touched.board ? <p className='text-red-600'>{errors.board}</p> : null}
                 </div>
                 <div className='input-group col-span-2 md:col-span-1'>
-                    <label className='input-label' htmlFor="yearGraduated">Year Graduated<span>*</span></label>
-                    <input className='input' type="date" name='yearGraduated' onBlur={handleBlur} onChange={handleChange} value={values.yearGraduated} />
-                    {errors.yearGraduated && touched.yearGraduated ? <p className='text-red-600'>{errors.yearGraduated}</p> : null}
+                    <label className='input-label' htmlFor="dateGraduated">Graduated date<span>*</span></label>
+                    <input className='input' type="date" name='dateGraduated' onBlur={handleBlur} onChange={handleChange} value={values.dateGraduated} />
+                    {errors.dateGraduated && touched.dateGraduated ? <p className='text-red-600'>{errors.dateGraduated}</p> : null}
                 </div>
                 <div className='input-group col-span-2 md:col-span-1'>
                     <label className='input-label' htmlFor="mark">{"Total Mark (%)"}<span>*</span></label>
@@ -70,11 +71,11 @@ const PageTwo = ({ isComplete }) => {
                 </div>
                 <div className='input-group col-span-2'>
                     <label className='input-label' htmlFor="award">Have you received any recognition, citation, or awards during school?</label>
-                    <textarea className='input' name="award" rows="4"/>
+                    <textarea className='input' name="award" rows="4" />
                 </div>
                 <div className='input-group col-span-2'>
                     <label className='input-label' htmlFor="participation">Have you participated in any organization in your school?</label>
-                    <textarea className='input' name="participation" rows="4"/>
+                    <textarea className='input' name="participation" rows="4" />
                 </div>
                 <button className='hidden' type='submit' ref={buttonRef}></button>
             </form>
